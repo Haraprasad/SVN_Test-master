72fa6d42ebfe50d6bce55b67d69b48c6bcdfd123
Haraprasad <suyog.haraprasad@gmail.com>
2015-11-20 19:30:25 +0530

    textview changes

--- app/src/main/java/com/suyogindia/svntest/Home.java
+++ app/src/main/java/com/suyogindia/svntest/Home.java
@@ -1,14 +1,18 @@
 package com.suyogindia.svntest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
+    TextView tvName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         //some changes
+        tvName = (TextView) findViewById(R.id.textView);
+        tvName.setText("asldbnksdfjg");
     }
 }
